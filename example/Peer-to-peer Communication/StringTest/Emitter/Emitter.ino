f9b8f5dc4084325475e4e3c3e59f2b6183c4e30d
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-04-26 09:23:46 +0900

    Updated example code

--- example/Peer-to-peer Communication/StringTest/Emitter/Emitter.ino
+++ example/Peer-to-peer Communication/StringTest/Emitter/Emitter.ino
@@ -2,8 +2,16 @@
 
 #include "Mirf.h"
 
+// for ATMega328
 Nrf24l Mirf = Nrf24l(10, 9); // CE,CSN
 
+// for RF-Nano
+//Nrf24l Mirf = Nrf24l(9, 10); // CE,CSN
+
+// for STM32
+//Nrf24l Mirf = Nrf24l(PB0, PB1); // CE,CSN
+
+ 
 union MYDATA_t {
   byte value[32];
   char now_time[32];
